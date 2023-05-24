2ef94725ed148d60eb6751ca7a650ecfce5af725
Liebus <e.eduards@inbox.lv>
2023-05-24 10:38:19 +0300

    Projektā tiek objekti funkcionāli pavilkti

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Jāiemportē, lai varētu lietot
+// visus I interfeisus
+using UnityEngine.EventSystems;
+public class DragDropSkripts : MonoBehaviour, IBeginDragHandler,
+IDragHandler, IEndDragHandler {
+ //Uzglabā norādi uz Objekti skriptu
+ public Objekti objektuSkripts;
+ private CanvasGroup kanvasGrupa;
+ private RectTransform velkObjRectTransf;
+
+ public void OnBeginDrag(PointerEventData eventData)
+ { 
+	objektuSkripts.pedejaisVilktais = null;
+	kanvasGrupa.alpha = 0.6f;
+	kanvasGrupa.blocksRaycasts = false;
+ }
+
+ public void OnDrag(PointerEventData eventData)
+ {
+		velkObjRectTransf.anchoredPosition +=
+			eventData.delta / objektuSkripts.kanva.scaleFactor;
+ }
+
+ public void OnEndDrag(PointerEventData eventData)
+ {
+		objektuSkripts.pedejaisVilktais = eventData.pointerDrag;
+		kanvasGrupa.alpha = 1f;
+
+		if(objektuSkripts.vaiIstajaVieta == false) {
+			kanvasGrupa.blocksRaycasts = true;
+		} else {
+			objektuSkripts.pedejaisVilktais = null;
+		}
+		objektuSkripts.vaiIstajaVieta = false;
+				
+ }
+
+	// Use this for initialization
+	void Start () {
+		//Piekļūst velkamā objekta CanvasGroup komponentei
+		kanvasGrupa = GetComponent<CanvasGroup>();
+		//Piekļūs velkamā objektya RectTransform komponentei
+		velkObjRectTransf = 
+		GetComponent<RectTransform>();
+	}
+	
+	
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour {
+ //Uzglabā visus velkamos objektus
+ public GameObject atkritumuMasina;
+ public GameObject atraPalidziba;
+ public GameObject autobuss;
+
+ //Uzglabā katra transportlīdzekļa sākotnējo atrašanās vietu
+ [HideInInspector]
+ public Vector2 atkrMKoord;
+ [HideInInspector]
+ public Vector2 atkrPKoord;
+
+  [HideInInspector]
+ public Vector2 bussKoord;
+
+ public Canvas kanva;
+
+ //Uzglabās audio avotu, kurā atskaņot audio efektus
+ public AudioSource skanasAvots;
+ //Uzglabā audio failus
+ public AudioClip[] skanaKoAtskanot;
+
+ [HideInInspector]
+ public bool vaiIstajaVieta = false;
+	public GameObject pedejaisVilktais = null;
+
+
+	// Use this for initialization
+	void Start () {
+		//Uzsākot spēli piefiksē kur sākotnēji atrodas katra mašīna
+		atkrMKoord = 
+		atkritumuMasina.GetComponent<RectTransform>().localPosition;
+
+		atkrPKoord = 
+		atraPalidziba.GetComponent<RectTransform>().localPosition;
+
+		bussKoord = 
+		autobuss.GetComponent<RectTransform>().localPosition;
+	}
+	
+	
+}
