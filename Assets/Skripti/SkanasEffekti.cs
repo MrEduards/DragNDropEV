7eb4ebd64b77594e3f0db9a288de2f9a5b080af0
E?V <liepa003@inbox.lv>
2023-06-06 23:13:07 +0300

    Tiek ieviests taimers un metodes, kurās tiek pievienotas attiecīgās dziesmas

---
+++ Assets/Skripti/SkanasEffekti.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkanasEffekti : MonoBehaviour
+{
+	public AudioClip[] dziesmuSaraksts;
+	public GameObject[] objektuSaraksts;
+	public Vector3[] vietuSaraksts;
+	public float[] lielumaSaraksts;
+
+	private bool[] objektuIevilkts;
+
+	private AudioSource audioAvots;
+
+	void Start()
+	{
+		audioAvots = GetComponent<AudioSource>();
+
+		// Inicializējam objektu ievilkšanas statusu
+		objektuIevilkts = new bool[objektuSaraksts.Length];
+	}
+
+	void Update()
+	{
+		// Pārbaudām katru objektu
+		for (int i = 0; i < objektuSaraksts.Length; i++)
+		{
+			GameObject objekts = objektuSaraksts[i];
+			Vector3 vieta = vietuSaraksts[i];
+			float lielums = lielumaSaraksts[i];
+
+			// Pārbaudām, vai objekts ir ievilkts atbilstošajā vietā ar attiecīgo izmēru
+			if (!objektuIevilkts[i] && objekts.transform.position == vieta && objekts.transform.localScale.x >= lielums)
+			{
+				objektuIevilkts[i] = true;
+				AtskaņotDziesmu(i);
+			}
+		}
+	}
+
+	void AtskaņotDziesmu(int indekss)
+	{
+		if (indekss >= 0 && indekss < dziesmuSaraksts.Length)
+		{
+			AudioClip dziesma = dziesmuSaraksts[indekss];
+			audioAvots.clip = dziesma;
+			audioAvots.Play();
+		}
+	}
+}
+
+
--- Assets/Skripti/Taimers.cs
+++ Assets/Skripti/Taimers.cs
@@ -1,29 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Taimers : MonoBehaviour 
+public class Taimers : MonoBehaviour
 {
 	float currentTime = 0;
-	float startingTime = 10f;
+	float startingTime = 180f; // 3 minūtes
 
-	[SerializeField] Texture TaimersText;
-	void Start ()
+	[SerializeField] Text taimeraTeksts;
+
+	void Start()
 	{
-		currentTime = startingTime;	
+		currentTime = startingTime;
 	}
-
 
 	void Update()
 	{
-		currentTime -= 1 * Time.deltaTime;
-		print (currentTime);
+		currentTime -= Time.deltaTime;
+		PārvaldītLaiku();
+		AtjauninātTaimeraTekstu();
 	}
 
+	void PārvaldītLaiku()
+	{
+		if (currentTime <= 0)
+		{
+			currentTime = 0;
+			// Šeit vari ievietot kodu, kas izpildās, kad taimers sasniedz 0
+		}
+	}
+
+	void AtjauninātTaimeraTekstu()
+	{
+		int minūtes = Mathf.FloorToInt(currentTime / 60);
+		int sekundes = Mathf.FloorToInt(currentTime % 60);
+		string formatētsLaiks = string.Format("{0:00}:{1:00}", minūtes, sekundes);
+		taimeraTeksts.text = formatētsLaiks;
+	}
 }
-
-
-
-
-
-
